cbb5231f4695aff884e043feaf9d3a853aa983d5
Anibal Ardid <anibal.ardid@intraway.com>
2016-06-23 13:55:35 -0300

    first commit

---
+++ www/js/app.js
@@ -0,0 +1,27 @@
+var app = angular.module('caffeinehit', [
+	'ionic',
+	'ngCordova',
+	'caffeinehit.controllers',
+	'caffeinehit.services',
+	'caffeinehit.filters'
+]);
+
+app.run(function ($ionicPlatform) {
+	$ionicPlatform.ready(function () {
+		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+		// for form inputs)
+		if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+			cordova.plugins.Keyboard.disableScroll(true);
+
+		}
+		if (window.StatusBar) {
+			// org.apache.cordova.statusbar required
+			StatusBar.styleDefault();
+		}
+	});
+});
+
+app.config(function ($httpProvider) {
+	$httpProvider.defaults.headers.common['Authorization'] = 'Token 1eb60143ffdb96dd6d0e8d56f25c27a6bb01df53';
+});
---
+++ www/js/services.js
@@ -0,0 +1,80 @@
+var app = angular.module('caffeinehit.services', []);
+
+app.service("YelpService", function ($q, $http, $ionicLoading, $cordovaGeolocation, $ionicPopup) {
+	var self = {
+		'page': 1,
+		'isLoading': false,
+		'hasMore': true,
+		'results': [],
+		'lat': 51.544440,
+		'lon': -0.022974,
+		'refresh': function () {
+			self.page = 1;
+			self.isLoading = false;
+			self.hasMore = true;
+			self.results = [];
+			return self.load();
+		},
+		'next': function () {
+			self.page += 1;
+			return self.load();
+		},
+		'load': function () {
+			self.isLoading = true;
+			var deferred = $q.defer();
+
+			ionic.Platform.ready(function () {
+				$cordovaGeolocation
+					.getCurrentPosition({timeout: 10000, enableHighAccuracy: false})
+					.then(function (position) {
+						self.lat = position.coords.latitude;
+						self.lon = position.coords.longitude;
+
+						var params = {
+							page: self.page,
+							lat: self.lat,
+							lon: self.lon
+						};
+
+
+						$http.get('http://ardid.com.ar/yelp/php/sample.php', {params: params})
+							.success(function (data) {
+								$ionicLoading.hide();
+								console.log(data);
+
+								if (data.qty == 0) {
+									self.hasMore = false;
+								} else {
+									angular.forEach(data.businesses, function (business) {
+										self.results.push(business);
+									});
+								}
+
+								self.isLoading = false;
+								deferred.resolve();
+							})
+							.error(function (data, status, headers, config) {
+								$ionicLoading.hide();
+								self.isLoading = false;
+								deferred.reject(data);
+							});
+
+					}, function (err) {
+						$ionicLoading.hide();
+						console.error("Error getting position");
+						console.error(err);
+						$ionicPopup.alert({
+							'title':'Por favor encienda la geolocalización',
+							'template': "Parece que tienes el gps desacticado. Por favor enciendalo y vuelva a ingresar a la aplicación."
+						});
+					})
+			});
+
+			return deferred.promise;
+		}
+	};
+
+	self.load();
+
+	return self;
+});
